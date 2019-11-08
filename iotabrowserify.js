af71a57b2d6a6d29e499ce4b6c80952b8c29e628
Thoralf-M <thoralf.mue@gmail.com>
2019-11-08 12:59:15 +0100

    add cda

--- iotabrowserify.js
+++ iotabrowserify.js
@@ -1,8 +1,11 @@
 //browserify version: 16.2.3, uglify version: uglify-js@3.5.15, iota.js version: @iota/core@1.0.0-beta.12
 
+//npm i -g browserify
+//npm i -g uglify-js
+
 //command: browserify iotabrowserify.js --standalone window| uglifyjs > iota.js-browser.js
 
-//license added on top of the resul
+//license added on top of the result
 
 global.bundle_validator = require('@iota/bundle-validator');
 global.bundle = require('@iota/bundle');
@@ -13,3 +16,4 @@ global.http_client = require('@iota/http-client');
 global.signing = require('@iota/signing');
 global.transaction_converter = require('@iota/transaction-converter');
 global.transaction = require('@iota/transaction');
+global.cda = require('@iota/cda');
